b5f5263b7350fa7382f45119d8934cbafc7dfa89
ayushm705 <42244325+ayushm705@users.noreply.github.com>
2020-11-19 23:20:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var navMenuAnchorTags=document.querySelectorAll('.nav-menu a');
- //console.log(navMenuAnchorTags);
+ console.log(navMenuAnchorTags);
 for(var i=0;i<navMenuAnchorTags.length;i++){
     navMenuAnchorTags[i].addEventListener('click',function(event){ 
     event.preventDefault();
@@ -16,6 +16,54 @@ for(var i=0;i<navMenuAnchorTags.length;i++){
             return;
         }
         window.scrollBy(0,50);
-    },50);
+    },20);
      });
 }
+
+
+var progressBars=document.querySelectorAll('.skill-progress >div');
+var skillsContainer=document.getElementById('skills-container');
+console.log(skillsContainer);
+window.addEventListener('scroll',checkScroll);
+var animationDone=false;
+
+function initializeBars()
+{
+    for(let bar of progressBars){
+        bar.style.width=0 +'%';
+    }
+}
+initializeBars();
+
+function fillBars(){
+    for(let bar of progressBars)
+    {
+        let targetWidth=bar.getAttribute('data-bar-width');
+        let currentWidth=0;
+        let interval=setInterval(function(){
+        if(currentWidth > targetWidth)
+        {
+            clearInterval(interval);
+            return;
+        }
+        currentWidth++;
+        bar.style.width=currentWidth +'%';
+        },1);
+    }
+}
+function checkScroll()
+{
+    var coordinates = skillsContainer.getBoundingClientRect();
+    if(!animationDone && coordinates.top<=window.innerHeight)
+    {
+    animationDone=true;
+    console.log("skills section visible");
+    fillBars();
+    }
+    else if(coordinates.top>window.innerHeight)
+    {
+        animationDone=false;
+        initializeBars();
+    }
+}
+
